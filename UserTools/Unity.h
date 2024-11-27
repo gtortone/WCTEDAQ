303c4d4e2731dde9a65ef90f8298c602f873217d
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-11-27 14:41:35 +0100

    remove unused tools

--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -1,16 +1,8 @@
 #include <Configuration.h>
-#include <DummyTool.h>
-#include <Dumper.h>
 #include <FileWriter.h>
-#include <HKMPMT.h>
 #include <JobManager.h>
-#include <LED.h>
 #include <Monitoring.h>
 #include <MPMTfakeTrigger.h>
 #include <MPMT.h>
-#include <Nhits.h>
 #include <RunControl.h>
 #include <SlaveRunControl.h>
-#include <Sorting.h>
-#include <Trigger.h>
-#include <WindowBuilder.h>
