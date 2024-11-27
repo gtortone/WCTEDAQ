2ba1349caded6dec36c3eaba0a40aa1ed295f9f9
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-11-27 11:58:02 +0100

    disable unused tools

--- UserTools/Digitizer/README.md
+++
@@ -1,2 +0,0 @@
-This is an abstract tool containing code commmon for V792 (QDC) and V1290 (TDC)
-digitizers.
--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -2,6 +2,7 @@
 #include <DummyTool.h>
 #include <Dumper.h>
 #include <FileWriter.h>
+#include <HKMPMT.h>
 #include <JobManager.h>
 #include <LED.h>
 #include <Monitoring.h>
@@ -12,12 +13,4 @@
 #include <SlaveRunControl.h>
 #include <Sorting.h>
 #include <Trigger.h>
-#include <V1290.h>
-#include <V1495.h>
-#include <V792.h>
-#include <V812.h>
-#include <VMEReceive.h>
-#include <VMESend.h>
-#include <VMETest.h>
 #include <WindowBuilder.h>
-#include <HKMPMT.h>
